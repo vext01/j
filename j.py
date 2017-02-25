a0059bc7c1f0874923c44cd14a0e527fbfc29d16
Edd Barrett <vext01@gmail.com>
2017-02-25 16:06:53 +0000

    Listing support.

--- j.py
+++ j.py
@@ -5,25 +5,36 @@ import sys
 import os
 import datetime
 import tempfile
+import time
 
 # XXX change default
 JRNL_DIR = os.environ.get("J_JOURNAL_PATH", os.path.abspath("j"))
 EDITOR = os.environ.get("EDITOR", "vi")
+TIME_FORMAT = "%Y%m%d_%H%M%S"
 
+RULE_SIZE = 78
+DOUBLE_RULE = "=" * RULE_SIZE
 
 class ParseError(Exception):
     pass
 
 class Entry:
-    def __init__(self, path):
+    def __init__(self, path, meta_only=False):
         self.path = path
         self.title = None
         self.date = None
         self.body = None
         self.tags = set()
-        self.parse()
+        self.parse(meta_only)
 
-    def parse(self):
+    def ident(self):
+        return os.path.basename(self.path)
+
+    def parse(self, meta_only=False):
+        # Get the time from the file path first
+        tstr = os.path.basename(self.path).split("-")[0]
+        self.date = datetime.datetime.strptime(tstr, TIME_FORMAT)
+
         with open(self.path) as fh:
             lines = iter(fh.readlines())
 
@@ -50,9 +61,28 @@ class Entry:
                 else:
                     pass  # blank attr line serves as the body separator
 
+                if meta_only:
+                    return
+
                 self.body = "".join(lines)
             except StopIteration:
                 raise ParseError("unexpected end of file")
+
+    def __str__(self):
+        date_str = str(self.date)
+        pad = " " * (RULE_SIZE - len(date_str) - len(self.ident()))
+        headers = [
+            DOUBLE_RULE,
+            "%s%s%s" % (date_str, pad, self.ident()),
+            self.title.center(RULE_SIZE),
+        ]
+        if self.tags:
+            atted_tags = ["@%s" % x for x in self.tags]
+            headers.append(" ".join(atted_tags).center(RULE_SIZE))
+        rec = "\n".join(headers)
+        if self.body:
+            rec += "\n\n%s" % self.body
+        return rec
 
 
 class Journal:
@@ -66,9 +96,8 @@ class Journal:
 
     def new_entry(self):
         now = datetime.datetime.now()
-        prefix = now.strftime("%Y%m%d_%H%M%S_")
-        fd, path = tempfile.mkstemp(dir=self.directory, prefix=prefix,
-                                    suffix=".txt")
+        prefix = now.strftime("%s-" % TIME_FORMAT)
+        fd, path = tempfile.mkstemp(dir=self.directory, prefix=prefix)
         os.close(fd)
         os.system("%s %s" % (EDITOR, path))
         try:
@@ -78,8 +107,24 @@ class Journal:
             print("parsing failed: %s" % e)
             sys.exit(1)
 
+    def show(self, bodies=False):
+        itr = os.scandir(self.directory)
+        files = sorted(filter(lambda x: x.is_file(), itr), key=lambda x: x.name)
+
+        if len(files):
+            for fl in files:
+                print("%s" % Entry(
+                    os.path.join(self.directory, fl.name), meta_only=not bodies))
 
 if __name__ == "__main__":
     logging.root.setLevel(logging.DEBUG)
     jrnl = Journal(JRNL_DIR)
-    jrnl.new_entry()
+
+    if len(sys.argv) == 1:  # no args
+        jrnl.new_entry()
+    else:
+        if sys.argv[1] == "ls":
+            jrnl.show()
+        elif sys.argv[1] == "show":
+            jrnl.show(bodies=True)
+
